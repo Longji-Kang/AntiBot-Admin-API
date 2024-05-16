5796d9dfbfee3f1cbbf524f221abf7f2da16a809
Longji Kang <kanglongjidev@gmail.com>
2024-05-16 21:17:59 +0200

    feat: add handler for upload function

--- lambda.py
+++ lambda.py
@@ -1,5 +1,12 @@
 import json
 import os
+import base64
+import boto3
+import time
+
+def upload_file(file_dir, target_file):
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(file_dir, os.environ['s3_bucket'], f"{os.environ['s3_folder']}/{target_file}")
 
 def handler(event, context):
 
@@ -23,8 +30,22 @@ def handler(event, context):
                 "body": '{"result": "invalid"}'
             }
     elif params['action'] == 'upload': 
-        a = 2
+        file_content = str(base64.b64decode(params['content']), encoding='ascii')
+        epoch        = int(time.time())
+        file_name    = f'{epoch}-definition.pkl'
+        file_dir     = f"/mnt/lambda/{file_name}"
 
+        file = open(file_dir, 'w')
+        file.write(file_content)
+        upload_file(file_dir, file_name)
+        
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": '{"content": "' + file_content + '"}'
+        }
     return {
         "statusCode": 200,
         "headers": {
