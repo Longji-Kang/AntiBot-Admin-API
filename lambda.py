ec431c437fd108461b6f50f13ee2e0f81253d2bf
Longji Kang <kanglongjidev@gmail.com>
2024-05-16 18:35:37 +0200

    fix: add mock login API

--- lambda.py
+++ lambda.py
@@ -1,8 +1,34 @@
+import json
+import os
+
 def handler(event, context):
+
+    params = json.loads(event['body'])
+
+    if params['action'] == 'login':
+        if params['username'] == 'admin' and params['password'] == os.environ['pass']:
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": '{"result": "approved"}'
+            }
+        else:
+            return {
+                "statusCode": 401,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": '{"result": "invalid"}'
+            }
+    elif params['action'] == 'upload': 
+        a = 2
+
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json"
         },
-        "body": "{\"result\": \"upload success\"}"
+        "body": event['body']
     }
